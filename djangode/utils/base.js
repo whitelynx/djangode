30f4ccc8cfa129b68a0e737074ecc8ee28ebda77
David H. Bronke <whitelynx@gmail.com>
2012-11-17 02:26:29 -0600

    Corrected utils/base.extend to check hasOwnProperty for each key.

--- djangode/utils/base.js
+++ djangode/utils/base.js
@@ -1,8 +1,8 @@
-
-exports.extend = function (original, extended){
-	for (var key in (extended || {})) { 
-        original[key] = extended[key];
+exports.extend = function (original, extended) {
+    for (var key in (extended || {})) {
+        if (Object.prototype.hasOwnProperty.call(extended, key)) {
+            original[key] = extended[key];
+        }
     }
-	return original;
+    return original;
 };
-
