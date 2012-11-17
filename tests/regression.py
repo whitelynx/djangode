55b045b951a5d479d8e0b60dcb0fb386f38ebaf4
David H. Bronke <whitelynx@gmail.com>
2012-11-17 15:23:53 -0600

    Reworked most of tests/regression.py.
    - it's more portable now (should work on Python3 and on Windows provided node is in your $PATH)
    - it now logs test output
    - it produces color output on non-Windows systems
    - it doesn't explode and refuse to proceed if the test script doesn't output the 'Totals: ..., Failures: ..., Errors: ...' line

--- tests/regression.py
+++ tests/regression.py
@@ -1,36 +1,73 @@
 #!/usr/bin/python
+from __future__ import print_function
+import os
+from os.path import dirname, join, relpath
+import platform
 import re
 from subprocess import Popen, PIPE
 
-print ""
 
-cmd = 'find . -name "*.test.js"'
-files = Popen(cmd, shell=True, stdout=PIPE).communicate()[0].splitlines()
+def out(msg, *args, **kwargs):
+    print(msg.format(*args, **kwargs))
+
+
+def error(msg, *args, **kwargs):
+    if platform.system() == 'Windows':
+        out(msg, *args, **kwargs)
+    else:
+        out('\033[1;31m{}\033[m'.format(msg), *args, **kwargs)
+
+
+print()
+
+resultLineRE = re.compile(
+        r'^\s*Total:\s*(\d+),\s*Failures:\s*(\d+),\s*Errors:\s*(\d+)\s*$',
+        re.IGNORECASE | re.MULTILINE
+        )
+
+testDir = dirname(__file__)
 failed_list = []
 
-for file in files:
-    output = Popen('node ' + file, shell=True, stdout=PIPE).communicate()[0].splitlines()
-    try:
-        result = [line for line in output if line.startswith('Total')][0]
-    except:
-        #bizarre, but sometimes popen apears to return empty strings
-        #I'm too tired to fix this right now, so for now just retry and hope for better results
-        output = Popen('node ' + file, shell=True, stdout=PIPE).communicate()[0].splitlines()
-        result = [line for line in output if line.startswith('Total')][0]
-    (total, failed, error) = re.split(r':|,', result)[1::2]
+for root, dirs, files in os.walk(testDir):
+    for testFile in sorted(files):
+        if not testFile.endswith('.test.js'):
+            continue
 
-    if int(failed) > 0 or int(error) > 0:
-        failed_list.append(file)
+        fullPath = join(root, testFile)
+        relativePath = relpath(fullPath, testDir)
+        out(relativePath)
 
-    print file
-    print '\t', result
+        output = Popen(['node', fullPath], stdout=PIPE).communicate()[0]
+
+        with open(fullPath + '.log', 'w') as testLog:
+            testLog.write(output)
+
+        match = resultLineRE.search(output)
+        if match:
+            total, failures, errors = match.groups()
+            failures, errors = int(failures), int(errors)
+
+            if failures > 0 or errors > 0:
+                failed_list.append((relativePath, failures, errors))
+                error('\t{}', match.group().strip())
+            else:
+                out('\t{}', match.group().strip())
+
+        else:
+            error('\tNo totals returned by test! Full output:\n{}', '\n'.join(output))
 
 if failed_list:
-    print '\nWARNING! There were failed tests:'
-    for file in failed_list:
-        print file
-    print ""
+    error('\nWARNING! There were failed tests:')
+    for testFile, failures, errors in failed_list:
+        errorSummary = []
+        if failures > 0:
+            errorSummary.append('{} failure{}'.format(failures, '' if failures == 1 else 's'))
+        if errors > 0:
+            errorSummary.append('{} error{}'.format(errors, '' if errors == 1 else 's'))
+
+        out('\t{:<30}  ({})', testFile, ', '.join((errorSummary)))
+    print()
     exit(1)
 
-print ""
+print()
 exit(0)
