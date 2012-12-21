df19d09141b4bc64f8c5f4ad6b8d74df8a9fe92d
David H. Bronke <dbronke@armt.com>
2012-12-21 10:29:29 -0600

    Fixed BaseError to correctly set up its message, and added new TemplateError class.

--- djangode/utils/errors.js
+++ djangode/utils/errors.js
@@ -1,6 +1,5 @@
 var util = require('util');
 
-
 // --------------------------------------------------------------------------------------------------------------------
 // Base Error
 // --------------------------------------------------------------------------------------------------------------------
@@ -9,10 +8,10 @@ function BaseError(name, level /*, message, ...*/)
 {
     this.name = name;
     this.level = level;
-    this.message = util.format.apply(this, Array.prototype.slice(arguments, [2]))
+    this.message = util.format.apply(this, Array.prototype.slice.call(arguments, 2))
     this.htmlMessage = util.format('<h2>%s</h2><p>%s</p>', name, this.message);
 
-    Error.call(this);
+    Error.call(this, this.message);
 }
 
 util.inherits(BaseError, Error);
@@ -85,10 +84,32 @@ util.inherits(NotIterable, BaseError);
 
 
 // --------------------------------------------------------------------------------------------------------------------
+// Template Error
+// --------------------------------------------------------------------------------------------------------------------
+
+function TemplateError(filename, token, message)
+{
+    var line = token.lineNum;
+    var col = token.column;
+
+    filename = filename || '<string>';
+
+    BaseError.call(this, "Template Error", "Critical", 'Template Error at %s:%d:%d: %s', filename, line, col, message);
+    this.htmlMessage = util.format(
+            '<h2>Template Error</h2><p><em>in %s on line %d, column %d</em></p><p>%s</p></ul>',
+            filename, line, col, message
+            );
+}
+
+util.inherits(TemplateError, BaseError);
+
+
+// --------------------------------------------------------------------------------------------------------------------
 
 module.exports = {
     'BaseError': BaseError,
     'FileNotFound': FileNotFound,
     'TemplateNotFound': TemplateNotFound,
     'NotIterable': NotIterable,
+    'TemplateError': TemplateError,
 };
